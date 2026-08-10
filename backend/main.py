cd250491e98eb41adea95de5ad551a45645b995e
16721237@mahasiswa.itb.ac.id <16721237@mahasiswa.itb.ac.id>
2026-08-10 16:13:54 +0700

    add: basic travel details in main.py

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+# Blok input
+
+destination = input("Masukkan destinasi: ")
+country = input("Masukkan negara: ")
+days = int(input("Masukkan jumlah hari: "))
+budget = float(input("Masukkan budget: "))
+currency = input("Masukkan mata uang: ")
+travel_month = input("Masukkan bulan perjalanan: ")
+
+print("========================")
+print("KelanaAI")
+print("========================\n")
+
+print(f"Destination : {destination}")
+print(f"Country     : {country}")
+print(f"Days        : {days}")
+print(f"Budget      : {budget} {currency}")
+print(f"Currency    : {currency}")
+print(f"Travel Month: {travel_month}")
